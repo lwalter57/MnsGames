7e058eb0a037f3ad6e1fe225744bfba5919dce33
lwalter57 <ludovic.walter@stagiairesmns.fr>
2023-06-19 11:54:56 +0200

    Ajoutez des fichiers projet.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MnsGames.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
